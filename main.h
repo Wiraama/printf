723aecfdae8630f2513ca74f1f11ee854685a25b
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-30 15:21:46 -0800

    priject

--- main.h
+++ main.h
@@ -1,6 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <math.h>
+#include <limits.h>
+
+/* Functions prototypes */
+
+int print_char(char c);
+int print_string(char *s);
 int _printf(const char *format, ...);
+int getNumberLength(int myNum);
+void check_number(int n);
 
 #endif
