02c23d26500988b02e216fce83c96b2e5ccf5705
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-30 14:23:24 -0800

    printf project

---
+++ quiz_0.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - main entry point
+ * @format: member
+ *
+ * Return: to the main function
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	int count = 0;
+	int c;
+	char *s;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		/** to check if the format is % to continue **/
+		if (*format == '%')
+		{
+			format++; /** moving to next character **/
+
+			if (*format == 'c') /** to check if next character is c **/
+			{
+				c = va_arg (args, int);
+
+				putchar (c);
+				count++;
+			}
+
+			/** to cheack if next format is s then loop if true **/
+			else if (*format == 's')
+			{
+				s = va_arg(args, char *);
+				while (*format != '\0')
+				{
+					putchar ('s');
+					s++;
+					count++;
+				}
+			}
+
+			/** to check if format is %(modulo) **/
+			else if (*format == '%')
+			{
+				putchar ('%');
+				count++;
+			}
+		}
+	}
+	va_end(args);
+ 
+	return (count);
+}
---
+++ quiz_1.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...)
+{
+
+	va_list (args);
+
+	int count = 0; /** initialiing to 0 **/
+	int a;
+
+	va_start(args, format);
+
+	/** ensurering character is not empty **/
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'd' || *format == 'i')
+			{
+				while(*format != '\0')
+				{
+					a = va_arg(args, int);
+					putchar (a + '0');
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar ('%');
+				count++;
+			}
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
+}
